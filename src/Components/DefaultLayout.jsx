e88d5c33c1245c7b90b5eb3a3e6830b486d83b4d
Naveennuwan <68013538+Naveennuwan@users.noreply.github.com>
2023-06-13 00:17:00 +0530

    PDF

--- src/Components/DefaultLayout.jsx
+++ src/Components/DefaultLayout.jsx
@@ -1,17 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-
 import { Outlet } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import MenuIcon from "@mui/icons-material/Menu";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import Avatar from "@mui/material/Avatar";
+import Logout from "@mui/icons-material/Logout";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 import Container from "@mui/material/Container";
@@ -19,16 +15,9 @@ import { logout } from "../Redux/UserRedux/UserActions";
 
 function DefaultLayout() {
   const dispatch = useDispatch();
-  const [logoutDropDown, setLogoutDropDown] = useState(null);
-
-  const openLogout = Boolean(logoutDropDown);
 
   const { userInfo } = useSelector((state) => state.userLogin);
   let navigate = useNavigate();
-
-  const handleLogoutMenuClick = (event) => {
-    setLogoutDropDown(event.currentTarget);
-  };
 
   const logoutHandler = () => {
     dispatch(logout(navigate));
@@ -64,7 +53,7 @@ function DefaultLayout() {
               </Typography>
               <Typography
                 variant="h6"
-              // className={`${routeInfo ? "appbar__header__pagename" : ""}`}
+                // className={`${routeInfo ? "appbar__header__pagename" : ""}`}
               >
                 {/* {routeInfo ? `${routeInfo}` : " "} */}
               </Typography>
@@ -86,37 +75,19 @@ function DefaultLayout() {
                 </Button>
               </Box>
 
-              <Avatar
-                alt="Remy Sharp"
-                src="/static/images/avatar/2.jpg"
-                sx={{ mr: 1 }}
-              />
-              <Typography textAlign="center" sx={{ mr: 1 }}>
+              <Typography textAlign="center" sx={{ mr: 2 }}>
                 {userInfo?.user.name}
               </Typography>
+
               <IconButton
                 size="large"
                 edge="start"
                 color="inherit"
-                onClick={handleLogoutMenuClick}
+                onClick={logoutHandler}
                 aria-label="open drawer"
               >
-                <MenuIcon></MenuIcon>
+                <Logout />
               </IconButton>
-              <Menu
-                id="basic-menu"
-                open={openLogout}
-                anchorEl={logoutDropDown}
-                MenuListProps={{
-                  "aria-labelledby": "basic-button",
-                }}
-              >
-                <Link to="/" style={{ textDecoration: "none" }}>
-                  <MenuItem onClick={logoutHandler} style={{ color: "black" }}>
-                    Logout
-                  </MenuItem>
-                </Link>
-              </Menu>
             </Toolbar>
           </Container>
         </AppBar>
